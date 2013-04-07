171cdac971be2fbebed75f5d59147ebee4f85a2a
Andy Sturges <arsturges@lbl.gov>
2013-04-06 23:55:41 -0700

    it's getting better.

--- craigslist_motorcycles.rb
+++ craigslist_motorcycles.rb
@@ -12,7 +12,6 @@ require 'open-uri'
 @motorcycles = Craig.query(
   :sf_bay_area, 
   :for_sale => :motorcycles)
-# If this were Python I would dump it into Pandas and filter it.
 
 # Put all results above minimum_price into @mcs
 minimum_price = 2000
@@ -21,6 +20,17 @@ minimum_price = 2000
   if motorcycle.price >= minimum_price
     @mcs.push(motorcycle)
   end
+end
+
+def convert_ForSale_to_hash(gem_results)
+  # gem result is an array of CraigsList::ForSale objects.
+  # Convert it to an array of hashes. Instead of obj.title
+  # now we have to do obj[:title]. Whatever. I need to hash methds.
+  @mcs_hashed = []
+  @mcs.each do |mc|
+    @mcs_hashed.push(mc.to_hash)
+  end
+  @mcs = @mcs_hashed
 end
 
 def get_brand(cl_title)
@@ -47,8 +57,17 @@ def get_brand_from_model(cl_title)
   # then use that to get the model.
   models = {
     "Heritage" => "Harley-Davidson",
+    "Sportster" => "Harley-Davidson",
+    "Dyna" => "Harley-Davidson",
     "gsxr" => 'Suzuki',
-    "ninja" => "Kawasaki"}
+    "ninja" => "Kawasaki",
+    "Maurader" => "Suzuki",
+    "Tiger" => "Triumph",
+    "Sabre" => "Honda",
+    "CBR" => "Honda",
+    "CR" => "Honda",
+    "zx6r" => "Kawasaki",
+    "Shadow" => "Honda"}
   result = nil
   models.keys.each do |model|
     if cl_title.downcase.include?(model.downcase)
@@ -67,21 +86,44 @@ end
 def get_engine_size(text)
 end
 
-def get_year(title,text)
+def get_list_of_years()
   text_years = []
   (00..12).each {|n| text_years.push("%02d" % n)}
-  (90..99).each {|n| text_years.push(n.to_s)}
-  years = (1990..2012).to_a.push(text_years).flatten
+  (80..99).each {|n| text_years.push(n.to_s)}
+  years = (1980..2012).to_a.push(text_years).flatten
 end
 
-@results = []
-n = 0
+def get_year(title,text, years)
+  result = nil
+  years.each do |year|
+    if title.include?(year.to_s)
+      result = year.to_i
+      break
+    end
+  end
+  if result.nil?
+    years.each do |year|
+      if text.include?(year.to_s)
+        result = year.to_i
+      end
+    end
+  end
+  # make it 4 digits:
+  if (0..13).member?(result)
+    result = 2000 + result
+  end
+    if (80..99).member?(result)
+      result = 1900 + result
+  end
+  return result
+end
+
+years = get_list_of_years()
+@mcs = convert_ForSale_to_hash(@mcs)
 @mcs.each do |mc|
-  @results[n] = {}
-  #scrape_body(mc.url)
-  @results[n]['URL'] = mc.url
-  @results[n]['price'] = mc.price
-  @results[n]['brand'] = get_brand(mc.title)
-  @results[n]['title'] = mc.title
-  n +=1
+  puts mc[:title]
+  mc[:brand] = get_brand(mc[:title])
+  text = get_body_text(mc[:url])
+  mc[:year] = get_year(mc[:title], text, years)
+  mc[:text] = text
 end
